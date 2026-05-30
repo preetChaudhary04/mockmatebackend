776eeaffa8ed0d2cfec7ed74a9b7bf2e690ab27c
preetChaudhary04 <preetxlm@gmail.com>
2026-05-30 22:08:08 +0530

    cors updated

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mockmate-gules.vercel.app"],
     credentials: true,
   }),
 );
